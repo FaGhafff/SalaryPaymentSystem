077ae38499e86697c9978462ff1214434b80cc9d
a-forghani-y79 <a.forghani.y79@gmail.com>
2020-05-10 19:12:18 +0430

    clean and rebuild

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public ImageView imageViewBack;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image image = new Image("PNG/PP.jpg");
+        imageViewBack.setImage(image);
+
+    }
 }
