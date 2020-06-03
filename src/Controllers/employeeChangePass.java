88d66c40316bc1ff7d2fd12b6a21c4f72293a8bd
FaGhafff <fatemeh.ghaufori@yahoo.com>
2020-06-03 16:46:16 +0430

    change pass & send massage

---
+++ src/Controllers/employeeChangePass.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import javafx.scene.image.Image;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class employeeChangePass implements Initializable {
+public ImageView c1 ;
+public ImageView ch2 ;
+public ImageView ch3 ;
+public Label chl1 ;
+public Label chl2 ;
+public Label chl3 ;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image im1 = new Image("PNG/key_26px.png");
+         ch1.
+        Image im2 = new Image("PNG/new_64px.png");
+        ch22.setImage(im2);
+        Image image3 = new Image("PNG/repet.png");
+        ch33.setImage(image3);
+
+
+    }
+}
